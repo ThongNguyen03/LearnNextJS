59ef758c2f88ba64e6fee90e3efe974d30ff6f20
ThongNguyen03 <nguyenbathong150203@gmail.com>
2025-06-11 16:13:16 +0700

    done invoices

--- app/dashboard/customers/page.tsx
+++ app/dashboard/customers/page.tsx
@@ -1,3 +1,9 @@
+import { Metadata } from 'next';
+ 
+export const metadata: Metadata = {
+  title: 'Customers | Acme Dashboard',
+  description: 'Manage your customers in the Acme Dashboard.',
+};
 export default function CustomersPage() {
   return (
     <div>
